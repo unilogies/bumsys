593b08be28e388484ea5aae93ad2fa87bba0737a
Khurshid Alam <kmkalam24@gmail.com>
2022-10-29 15:47:24 +0600

    Solve counting issue

--- module/accounts/overview.php
+++ module/accounts/overview.php
@@ -9,25 +9,78 @@
 
     <?php 
       
-      $overview = easySelectD("
-        select 
-          db_date, 
-          if(sales_due_sum is null, 0, sales_due_sum) as receivables_amount, 
-          ( ( if(salary_amount_sum is null, 0, salary_amount_sum) + if(bills_amount_sum is null, 0, bills_amount_sum) ) - if(emp_com_payment_amount_sum is null, 0, emp_com_payment_amount_sum) ) as payables_amount,
-          ( ( if(sales_grand_sum is null, 0, sales_grand_sum) - if(sales_due_sum is null, 0, sales_due_sum) ) + if(received_payments_amount_sum is null, 0, received_payments_amount_sum) + if(incomes_amount_sum is null, 0, incomes_amount_sum) ) as total_income, 
-          ( if(payment_amount_sum is null, 0, payment_amount_sum) + if(loan_amount_sum is null, 0, loan_amount_sum) ) as total_expence 
-        from time_dimension
-          left join ( select sales_delivery_date, sum(sales_grand_total) as sales_grand_sum, sum(sales_due) as sales_due_sum from {$table_prefeix}sales group by sales_delivery_date ) as get_sales on sales_delivery_date = db_date
-          left join ( select received_payments_add_on, sum(received_payments_amount) as received_payments_amount_sum from {$table_prefeix}received_payments group by date(received_payments_add_on) ) as get_received_payments on date(received_payments_add_on) = db_date
-          left join ( select incomes_date, sum(incomes_amount) as incomes_amount_sum from {$table_prefeix}incomes group by incomes_date ) as get_income on incomes_date = db_date
-          left join ( select payment_date, sum(payment_amount) as payment_amount_sum from {$table_prefeix}payments group by payment_date ) as get_payments on get_payments.payment_date = db_date
-          left join ( select loan_pay_on, sum(loan_amount) as loan_amount_sum from {$table_prefeix}loan group by date(loan_pay_on) ) as get_loan on date(loan_pay_on) = db_date
-          left join ( select salary_update_on, sum(salary_amount) as salary_amount_sum from {$table_prefeix}salaries group by date(salary_update_on) ) as get_salaries on date(salary_update_on) = db_date
-          left join ( select payment_date, sum(payment_amount) as emp_com_payment_amount_sum from {$table_prefeix}payments where payment_to_company is not null or payment_to_employee is not null group by payment_date ) as get_com_emp_payments on get_com_emp_payments.payment_date = db_date
-          left join ( select bills_date, sum(bills_amount) as bills_amount_sum from {$table_prefeix}bills group by bills_date ) as get_bills on bills_date = db_date
-        where month(CURRENT_DATE) = month(db_date) and year(CURRENT_DATE) = year(db_date)
-        order by db_date ASC
-      ")["data"];
+        $overview = easySelectD("
+            SELECT 
+                db_date, 
+
+                if(sales_due_sum is null, 0, sales_due_sum) as receivables_amount, 
+                (   ( if(salary_amount_sum is null, 0, salary_amount_sum) + if(bills_amount_sum is null, 0, bills_amount_sum) ) - 
+                    if(emp_com_payment_amount_sum is null, 0, emp_com_payment_amount_sum) 
+                ) as payables_amount,
+
+                (   (if(sales_grand_sum is null, 0, sales_grand_sum) - if(sales_due_sum is null, 0, sales_due_sum) ) + 
+                    if(received_payments_amount_sum is null, 0, received_payments_amount_sum) + 
+                    if(incomes_amount_sum is null, 0, incomes_amount_sum) 
+                ) as total_income, 
+
+                if(payment_amount_sum is null, 0, payment_amount_sum) + if(loan_amount_sum is null, 0, loan_amount_sum) as total_expence 
+
+            FROM time_dimension
+
+            left join ( select 
+                    sales_delivery_date, 
+                    sum(sales_grand_total) as sales_grand_sum, 
+                    sum(sales_due) as sales_due_sum 
+                from {$table_prefeix}sales 
+                where is_trash = 0
+                group by sales_delivery_date 
+            ) as get_sales on sales_delivery_date = db_date
+            left join ( select 
+                    received_payments_datetime, 
+                    sum(received_payments_amount) as received_payments_amount_sum 
+                from {$table_prefeix}received_payments 
+                group by date(received_payments_datetime) 
+            ) as get_received_payments on date(received_payments_datetime) = db_date
+            left join ( select 
+                    incomes_date, 
+                    sum(incomes_amount) as incomes_amount_sum 
+                from {$table_prefeix}incomes 
+                group by incomes_date 
+            ) as get_income on incomes_date = db_date
+            left join ( select 
+                    payment_date, 
+                    sum(payment_amount) as payment_amount_sum 
+                from {$table_prefeix}payments 
+                group by payment_date 
+            ) as get_payments on get_payments.payment_date = db_date
+            left join ( select 
+                    loan_pay_on, 
+                    sum(loan_amount) as loan_amount_sum 
+                from {$table_prefeix}loan group 
+                by date(loan_pay_on) 
+            ) as get_loan on date(loan_pay_on) = db_date
+            left join ( select 
+                    salary_update_on, 
+                    sum(salary_amount) as salary_amount_sum 
+                from {$table_prefeix}salaries 
+                group by date(salary_update_on) 
+                ) as get_salaries on date(salary_update_on) = db_date
+            left join ( select 
+                    payment_date, 
+                    sum(payment_amount) as emp_com_payment_amount_sum 
+                from {$table_prefeix}payments 
+                where payment_to_company is not null or payment_to_employee is not null 
+                group by payment_date 
+            ) as get_com_emp_payments on get_com_emp_payments.payment_date = db_date
+            left join ( select 
+                    bills_date, 
+                    sum(bills_amount) as bills_amount_sum 
+                from {$table_prefeix}bills
+                group by bills_date 
+            ) as get_bills on bills_date = db_date
+            where month(CURRENT_DATE) = month(db_date) and year(CURRENT_DATE) = year(db_date)
+            order by db_date ASC
+        ")["data"];
 
       $dates = array();
       $incomes = array();
